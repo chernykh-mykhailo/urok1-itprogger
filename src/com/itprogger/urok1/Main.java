38868dd83d3fd8a69d610ed35866eed85b45d43c
chernykh-mykhailo <270775mik@gmail.com>
2022-02-23 02:37:49 +0200

    urok1, video 3/23

---
+++ src/com/itprogger/urok1/Main.java
@@ -0,0 +1,15 @@
+package com.itprogger.urok1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // \-важлива штука, помога символи юзати в строкі
+        /*
+        дуже класно й весело 2.22.2022
+         */
+        System.out.println("Good bye");
+        System.out.println("Good \t bye");
+        System.out.println("Good \"\\bye\n \n baby");
+    }
+
+}
